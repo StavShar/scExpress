33a22fd7fe7655601de360b32ddce0ad50f6dc41
st sh <stavsh2@ac.sce.ac.il>
2021-11-25 15:00:29 +0200

    .

--- scExperess/Orders.h
+++ scExperess/Orders.h
@@ -24,11 +24,11 @@ orders* Get_All_Waiting_Orders(orders* list, int* size);
 orders* Set_All_Waiting_Orders(orders* list, int* size);
 orders* Add_Order(orders* list, int* size, orders order);
 orders* Remove_Order(orders* list, int* size, int orderSN);
-
 int DailyProfit(float* pTotalPrice, float tp);
 void PrintfProfit(int* pTotalPrice);
+int Get_New_Order_SN();
+
 orders MakeOrder(ProductFile* listPro, int* sizep, int orderSN, char* username, int id, char status);
 void ViewOrders(orders* ListOrders, int* ptr);
 orders* ChangeStatus(orders* Allorders);
 void orderHistory(int id, ProductFile* order, int items, char status, int* ptr, float* pTotalPrice);
-int Get_New_Order_SN();
