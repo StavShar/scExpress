6a79ac364ee1b7e59f9287953a8485a93ee570d9
Sharon <sharon@gmail.com>
2021-11-25 12:01:08 +0200

    sharon

--- scExperess/searchProduct.h
+++ scExperess/searchProduct.h
@@ -6,4 +6,4 @@ int Serial_num_search(Product* list,int size ,int num);
 void Print_Rating(int total_rate, int rating_count);
 void Website_ranking(int* total ,int* count);
 void Low_to_high(Product* list,int size);
-
+void Select_cat(Product* list,int size);
