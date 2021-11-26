f4d353c099779f0741e6818669be3f9270175e5f
st sh <stavsh2@ac.sce.ac.il>
2021-11-26 16:00:26 +0200

    update

--- scExperess/menu.h
+++ scExperess/menu.h
@@ -9,9 +9,10 @@ void ManagerEntranceLoop(Manager* managers, int* size, float* profit);
 void printManagerEntranceOptions();//A function that print the menu to screen.
 void ClientEntranceLoop(Client* clients, int* size);
 void printClientEntranceOptions();//A function that print the menu to screen.
-void ManagerLoop(float* profit);
+void ManagerLoop(float* profit, Manager m);
 void printManagerOptions();//A function that print the menu to screen.
-void ClientLoop();
+void ClientLoop(Client c);
+void printCartMenu();
 void printClientOptions();//A function that print the menu to screen.
 void Searches(Product* products, int size);//sub menu of different searches
 void Get_Rating_vars(int* tr, int* rcount);
