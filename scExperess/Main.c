e7188d8a69531b5d2b608eebe118a7c4753e5c5e
st sh <stavsh2@ac.sce.ac.il>
2022-01-08 14:07:01 +0200

    update

--- scExperess/Main.c
+++ scExperess/Main.c
@@ -6,12 +6,3 @@ int main()
 	mainMenu();
 }
 
-/*
-search product sub menu return option (manager) ->> fixed without test doc.
-
-if you add to cart 2 times from the same products it will add it to the cart as 2 separate products ->> fixed but didnt check yet if any test doc. need to adjust
-
-total price not updated after removing product ->> fixed
-
-removing all product then add something leads for crash ->> fixed
-*/
